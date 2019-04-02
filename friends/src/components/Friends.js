437b0ccadf9f7aeb56925d7e7aca7c946d48f92d
JamesB <jblazer1@yahoo.com>
2019-04-01 20:24:29 -0700

    Added occupation key to friends array in server.js and code to display occupation on page

--- friends/src/components/Friends.js
+++ friends/src/components/Friends.js
@@ -4,12 +4,13 @@ const Friends = props => {
   //   console.log(props.friends);
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello Hello</h1>
       {props.friends.map(friend => (
         <div key={friend.id}>
           <h1>{friend.name}</h1>
           <h3>{friend.age}</h3>
           <h3>{friend.email}</h3>
+          <h3>{friend.occupation}</h3>
         </div>
       ))}
     </div>
